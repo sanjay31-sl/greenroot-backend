68da17544bf73109c0c6bd17590cb530c11155cc
SANJAY S .L <sanjayslsanju333@gmail.com>
2026-03-16 22:07:42 +0530

    Update CORS middleware to allow multiple origins

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'https://greenroot-frontend.vercel.app'], credentials: true }));
 app.use(express.json());
 
 // Routes
